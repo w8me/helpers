08234fc007a13c59219cc2d2f8672af795a435b7
reLUCKs <novaknazar@gmail.com>
2019-08-26 10:59:53 +0300

    facebook helper

---
+++ helpers/src/main/java/com/relucks/helpers/FacebookHelper.java
@@ -0,0 +1,27 @@
+package com.relucks.helpers;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.util.Base64;
+import android.util.Log;
+
+import java.security.MessageDigest;
+
+public class FacebookHelper {
+
+    public static void generateKeyHashes(Context context) {
+        try {
+            @SuppressLint("PackageManagerGetSignatures")
+            PackageInfo info = context.getPackageManager().getPackageInfo(context.getPackageName(), PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (Exception ignored) {
+        }
+    }
+}
